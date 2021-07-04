235ddc3c425d4ce88bfd7a90e811037d45b63826
unknown <boninoffranco@gmail.com>
2021-07-04 11:34:08 -0300

    Translate and rewrite code

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,46 +15,42 @@ function App() {
     setTimeout(() => setCities(oldCities => oldCities.filter(c => c.id !== id)), 500);  
   }
 
-  function onSearch(ciudad) {
-    fetch(`https://api.openweathermap.org/data/2.5/find?q=${ciudad}&appid=${apiKey}&units=metric`)
+  function onSearch(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/find?q=${city}&appid=${apiKey}&units=metric`)
       .then(r => r.json())
-      .then((recurso) => {
-        if(recurso.count){
-          recurso = recurso.list[0];
+      .then(data => {
+        if(data.count){
+          data = data.list[0];
           const city = {
-            min: Math.round(recurso.main.temp_min),
-            max: Math.round(recurso.main.temp_max),
-            img: recurso.weather[0].icon,
-            id: recurso.id,
-            wind: recurso.wind.speed,
-            temp: recurso.main.temp,
-            name: recurso.name,
-            weather: recurso.weather[0].main,
-            clouds: recurso.clouds.all,
-            latitud: recurso.coord.lat,
-            longitud: recurso.coord.lon,
-            country: recurso.sys.country,
-            feelsLike: recurso.main.feels_like
+            min: Math.round(data.main.temp_min),
+            max: Math.round(data.main.temp_max),
+            img: data.weather[0].icon,
+            id: data.id,
+            wind: data.wind.speed,
+            temp: data.main.temp,
+            name: data.name,
+            weather: data.weather[0].main,
+            clouds: data.clouds.all,
+            latitud: data.coord.lat,
+            longitud: data.coord.lon,
+            country: data.sys.country,
+            feelsLike: data.main.feels_like
           };
-          cities.filter(oldCity => oldCity.id === city.id).length ? alert("Ciudad ya agregada") : setCities(oldCities => [...oldCities, city]);
+          cities.filter(oldCity => oldCity.id === city.id).length ? alert("City already added") : setCities(oldCities => [...oldCities, city]);
         } else {
-          alert("Ciudad no encontrada");
+          alert("City not found");
         }
       });
   }
 
-  function onFilter(ciudadId) {
-    let ciudad = cities.filter(c => c.id === parseInt(ciudadId));
-    if(ciudad.length > 0) {
-        return ciudad[0];
-    } else {
-        return null;
-    }
+  function onFilter(cityId) {
+    let city = cities.filter(c => c.id === parseInt(cityId));
+    return city.length ? city[0] : null;
   }
 
   return (
     <div className="App">
-      <Route exact path = '/'>
+      <Route path = '/'>
         <Nav onSearch = {onSearch}/>
       </Route>
       <Route path='/about' component={About}/>
