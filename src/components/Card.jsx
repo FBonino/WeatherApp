5c072fda50ab18abba1c645b528c90e2afca3c24
unknown <boninoffranco@gmail.com>
2021-07-31 12:32:40 -0300

    Card style fixes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Card.module.css'
 
-export default function Card ({name, img, onClose, id, country, temp}) {
+export default function Card({ name, img, onClose, id, country, temp }) {
   return (
     <div className={styles.card}>
       <button className={styles.button} onClick={onClose}> 🔴 </button>
-      <Link to = {`/city/${id}`} className={styles.link}>
-        <div>        
+      <Link to={`/city/${id}`} className={styles.link}>
+        <div>
           <div className={styles.city}>
-            <h3> {name}, {country}</h3>
-            <img className={styles.flag} src={`https://openweathermap.org/images/flags/${country.toLowerCase()}.png`} alt=""/>
+            <h3 style={{ overflow: 'clip' }}> {name}, {country}</h3>
+            <img className={styles.flag} src={`https://openweathermap.org/images/flags/${country.toLowerCase()}.png`} alt="" />
           </div>
         </div>
         <div className={styles.props}>
@@ -18,10 +18,10 @@ export default function Card ({name, img, onClose, id, country, temp}) {
             <h2> {temp} ºC</h2>
           </div>
           <div>
-            <img className={styles.image} src={`https://openweathermap.org/img/wn/${img}@2x.png`} alt=""/>
-          </div>              
+            <img className={styles.image} src={`https://openweathermap.org/img/wn/${img}@2x.png`} alt="" />
+          </div>
         </div>
       </Link>
-    </div> 
+    </div>
   );
 };
